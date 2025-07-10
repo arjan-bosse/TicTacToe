20977cffe24e43a599bf30286e54bc6f77d5d7d8
Arjan Bosse <abosse@planet.nl>
2025-07-10 12:02:59 +0200

    initial project version

---
+++ TicTacToe/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// TicTacToe (User Interface)
+// Versie: 0.1
+// Datum : 2014-08-10
+// Auteur: Arjan Bosse
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        // Het lopende TicTacToe spel
+        private T3State ttt;
+
+        // Font voor winnende buttons, maak tekst 2x zo groot en bold
+        private Font winFont;
+        private Font oldFont;
+
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        //  Maak spel aan en refresh buttons
+        //
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            // Maak spel aan
+            ttt = new T3State();
+
+            // Font voor buttons bij winst
+            oldFont = button10.Font;
+            winFont = new Font(button10.Font.FontFamily, button10.Font.Size * 2, FontStyle.Bold);
+
+            // Buttons bijwerken
+            refresh();
+        }
+
+        // Spel status is gewijzigd, werk de buttons bij
+        //
+        private void refresh()
+        {
+            String[] bord = ttt.getBord();
+            String winst = ttt.getWinst();
+            String winnaar = ttt.getWinnaar();
+
+            // Buttons fonts resetten
+            button1.Font = oldFont;
+            button2.Font = oldFont;
+            button3.Font = oldFont;
+            button4.Font = oldFont;
+            button5.Font = oldFont;
+            button6.Font = oldFont;
+            button7.Font = oldFont;
+            button8.Font = oldFont;
+            button9.Font = oldFont;
+
+            // Buttons teksten
+            button1.Text = bord[0]; 
+            button2.Text = bord[1];
+            button3.Text = bord[2];
+            button4.Text = bord[3];
+            button5.Text = bord[4];
+            button6.Text = bord[5];
+            button7.Text = bord[6];
+            button8.Text = bord[7];
+            button9.Text = bord[8];
+
+            // Buttons kleuren
+            button1.ForeColor = (bord[0] == "X") ? Color.Red : Color.Green;
+            button2.ForeColor = (bord[1] == "X") ? Color.Red : Color.Green;
+            button3.ForeColor = (bord[2] == "X") ? Color.Red : Color.Green;
+            button4.ForeColor = (bord[3] == "X") ? Color.Red : Color.Green;
+            button5.ForeColor = (bord[4] == "X") ? Color.Red : Color.Green;
+            button6.ForeColor = (bord[5] == "X") ? Color.Red : Color.Green;
+            button7.ForeColor = (bord[6] == "X") ? Color.Red : Color.Green;
+            button8.ForeColor = (bord[7] == "X") ? Color.Red : Color.Green;
+            button9.ForeColor = (bord[8] == "X") ? Color.Red : Color.Green;
+
+            // Winnende buttons highlighten
+            if (winst != "")
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (winst[i] == '1') { button1.Font = winFont; }
+                    if (winst[i] == '2') { button2.Font = winFont; }
+                    if (winst[i] == '3') { button3.Font = winFont; }
+                    if (winst[i] == '4') { button4.Font = winFont; }
+                    if (winst[i] == '5') { button5.Font = winFont; }
+                    if (winst[i] == '6') { button6.Font = winFont; }
+                    if (winst[i] == '7') { button7.Font = winFont; }
+                    if (winst[i] == '8') { button8.Font = winFont; }
+                    if (winst[i] == '9') { button9.Font = winFont; }
+                }
+            }
+
+            // Status button
+            if (winnaar != "")
+            {
+                button10.Text = "Winnaar " + winnaar;
+            }
+            else if (ttt.isGelijkspel())
+            {
+                button10.Text = "Gelijkspel";
+            }
+            else
+            {
+                button10.Text = "Speel " + ttt.getPlayer();
+            }
+
+            // Buttons verversen
+            button1.Invalidate();
+            button2.Invalidate();
+            button3.Invalidate();
+            button4.Invalidate();
+            button5.Invalidate();
+            button6.Invalidate();
+            button7.Invalidate();
+            button8.Invalidate();
+            button9.Invalidate();
+            button10.Invalidate();
+        }
+
+        // Gebruiker speelt zelf
+        // 
+        private void button1_Click(object sender, EventArgs e) { button1.Text = ttt.speel(1); refresh(); }
+        private void button2_Click(object sender, EventArgs e) { button2.Text = ttt.speel(2); refresh(); }
+        private void button3_Click(object sender, EventArgs e) { button3.Text = ttt.speel(3); refresh(); }
+        private void button4_Click(object sender, EventArgs e) { button4.Text = ttt.speel(4); refresh(); }
+        private void button5_Click(object sender, EventArgs e) { button5.Text = ttt.speel(5); refresh(); }
+        private void button6_Click(object sender, EventArgs e) { button6.Text = ttt.speel(6); refresh(); }
+        private void button7_Click(object sender, EventArgs e) { button7.Text = ttt.speel(7); refresh(); }
+        private void button8_Click(object sender, EventArgs e) { button8.Text = ttt.speel(8); refresh(); }
+        private void button9_Click(object sender, EventArgs e) { button9.Text = ttt.speel(9); refresh(); }
+
+        // Gebruiker laat computer spelen
+        //
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int advies = ttt.advies();
+            if (advies == 1) { button1.Text = ttt.speel(1); }
+            if (advies == 2) { button2.Text = ttt.speel(2); }
+            if (advies == 3) { button3.Text = ttt.speel(3); }
+            if (advies == 4) { button4.Text = ttt.speel(4); }
+            if (advies == 5) { button5.Text = ttt.speel(5); }
+            if (advies == 6) { button6.Text = ttt.speel(6); }
+            if (advies == 7) { button7.Text = ttt.speel(7); }
+            if (advies == 8) { button8.Text = ttt.speel(8); }
+            if (advies == 9) { button9.Text = ttt.speel(9); }
+            refresh();
+        }
+
+        // Neem zet terug
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ttt.terug();
+            refresh();
+        }
+
+        // Neem alle zetten terug
+        private void button12_Click(object sender, EventArgs e)
+        {
+            ttt.reset();
+            refresh();
+        }
+    }
+}
---
+++ TicTacToe/T3State.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// TicTacToe (Game Logic)
+// Versie: 0.1
+// Datum : 2014-08-14
+// Auteur: Arjan Bosse
+
+namespace TicTacToe
+{
+    // Het lopende TicTacToe spel
+    //
+    class T3State
+    {
+        private String[] bord;
+        private String aanzet;
+        private String winnaar;
+        private String winst;
+        private int zettenGedaan;
+        private int[] voorkeur;
+        private int[] history;
+
+        // Conctructor
+        //
+        public T3State()
+        {
+            bord = new String[9] { "", "", "", "", "", "", "", "", "" };
+            aanzet = "X";
+            winnaar = "";
+            winst = "";
+            zettenGedaan = 0;
+            history = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            voorkeur = new int[9] { 4, 0, 8, 2, 6, 1, 7, 3, 5 };
+        }
+
+        // Maak laatste zet ongedaan
+        // Retourneer veld waarop was gespeeld
+        //
+        public int terug()
+        {
+            int veld = 0;
+            if (zettenGedaan > 0)
+            {
+                zettenGedaan--;
+                veld = history[zettenGedaan] + 1;
+                bord[veld-1] = "";
+                history[zettenGedaan] = 0;
+                winnaar = "";
+                winst = "";
+                aanzet = (aanzet == "X") ? "O" : "X";
+            }
+            return veld;
+        }
+
+        // Maak alle zetten ongedaan
+        //
+        public void reset()
+        {
+            while (terug() > 0) { }
+        }
+
+        // Nagaan of de gedane zet winstgevend is
+        // Retourneer winnende velden
+        //
+        private String getUitslag(String gezet)
+        {
+            // Winst horizontaal
+            if (gezet == bord[0] && bord[0] == bord[1] && bord[1] == bord[2]) return "123";
+            if (gezet == bord[3] && bord[3] == bord[4] && bord[4] == bord[5]) return "456";
+            if (gezet == bord[6] && bord[6] == bord[7] && bord[7] == bord[8]) return "789";
+
+            // Winst verticaal
+            if (gezet == bord[0] && bord[0] == bord[3] && bord[3] == bord[6]) return "147";
+            if (gezet == bord[1] && bord[1] == bord[4] && bord[4] == bord[7]) return "258";
+            if (gezet == bord[2] && bord[2] == bord[5] && bord[5] == bord[8]) return "369";
+
+            // Winst diagonaal
+            if (gezet == bord[0] && bord[0] == bord[4] && bord[4] == bord[8]) return "159";
+            if (gezet == bord[2] && bord[2] == bord[4] && bord[4] == bord[6]) return "357";
+
+            // Geen winst
+            return "";
+        }
+
+        // Get functies
+        //
+        public String getPlayer() { return aanzet; }
+        public String getWinnaar() { return winnaar; }
+        public String getWinst() { return winst; }
+        public Boolean isGelijkspel() { return winnaar == "" && zettenGedaan == 9; }
+        public String[] getBord() { return bord; }
+
+        // Speel veld en werk status bij
+        // Retourneer gespeelde zet
+        //
+        public String speel(int veld)
+        {
+            // Als zet niet mogelijk dan retourneer leeg
+            if (winnaar != "") return "";
+            if (veld < 1 || veld > 9) return "";
+            if (bord[veld-1] != "") return "";
+
+            // Er is een zet gedaan, werk bij
+            bord[veld-1] = aanzet;
+
+            // Geschiedenis
+            history[zettenGedaan] = veld-1;
+
+            // Na 9 zetten zonder winst is het gelijkspel
+            zettenGedaan++;
+
+            // Ga na or er een winst is
+            winst = getUitslag(aanzet);
+
+            // Winnaar
+            if (winst != "") winnaar = aanzet;
+
+            // Volgende aan zet
+            aanzet = (aanzet == "O") ? "X" : "O";
+
+            // Gespeelde kleur
+            return bord[veld-1];
+        }
+
+        //  Datatype voor analyse methode
+        //
+        private struct Result
+        {
+            public int veld;
+            public int score;
+        }
+
+        // Geef beste zet en score terug
+        // Bord en gedane zetten van het lopende spel worden gebruikt
+        // Na afloop zijn alle bijwerkingen weer ongedaan gemaakt
+        // Achtergrond: dit is een recursieve implementatie van het minimax algoritme
+        //
+        private Result analyse(String speler)
+        {
+            Result r = new Result();
+            String tegenstander = (speler == "X") ? "O" : "X";
+
+            // Als tegenstander een winnende zet heeft gedaan, dan heeft speler verloren
+            // Laat de mate van verlies of winst afhangen van het aantal gespeelde zetten
+            // Zodoende probeert de winnaar snel te winnen en de verliezer langzaam te verliezen
+            String w = getUitslag(tegenstander);
+            if (w != "") { r.veld = 0; r.score = zettenGedaan - 10; return r; }
+
+            // Als geen zet mogelijk, dan gelijkspel
+            if (zettenGedaan == 9) { r.veld = 0; r.score = 0; return r; }
+
+            // Zoek beste zet voor speler, dat is de zet die het slechtst scoort bij de tegenstander
+            r.veld = 0;
+            r.score = -10; // erger dan maximaal verlies, dan is er altijd een betere zet
+
+            // Probeer alle beschikbare velden
+            for (int i = 0; i < 9; i++)
+            {
+                // Heuristiek: eerst het midden, dan de hoeken, tenslotte de zijkanten
+                int j = voorkeur[i];
+
+                // Als leeg veld dan speel en ga na of dit een goede zet is
+                if (bord[j] == "")
+                {
+                    // Doe zet voor speler
+                    bord[j] = speler;
+                    zettenGedaan++;
+
+                    // Speler heeft gezet, nu is de tegenstander aan de beurt
+                    // Bepaal score gezien vanuit de tegenstander (recursieve aanroep)
+                    Result r2 = analyse(tegenstander);
+
+                    // Draai teken van de score om (+ is winst, - is verlies, 0 is gelijkspel)
+                    // Een negatieve score bij de tegenstander is een positieve score bij de speler
+                    r2.score = -r2.score;
+
+                    // Zet onthouden als deze beter is dan de beste tot nu toe
+                    if (r2.score > r.score) { r.score = r2.score; r.veld = j + 1; }
+
+                    // Neem zet terug, de speler moet de volgende proberen
+                    zettenGedaan--;
+                    bord[j] = "";
+                }
+            }
+
+            // Geef beste zet en score terug
+            return r;
+        }
+
+        // Adviseer een zet
+        // Retourneer een veld
+        //
+        public int advies()
+        {
+            // Bord leeg, zet willekeurig
+            if (zettenGedaan == 0)
+            {
+                return new Random().Next(1, 9);
+            }
+
+            // Zoek beste zet
+            Result r = analyse(aanzet);
+            return r.veld;
+        }
+    }
+}
